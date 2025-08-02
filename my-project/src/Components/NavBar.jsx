c36296a2d793f649a83453f7158a76fae38ab5b8
NAVEEN KUMAR <nk946432@gmail.com>
2025-08-02 19:32:06 +0530

    adjusted a side navbar_!

--- my-project/src/Components/NavBar.jsx
+++ my-project/src/Components/NavBar.jsx
@@ -68,8 +68,8 @@ const NavBar = () => {
 
       {/* Mobile Drawer */}
       <div
-        className={`fixed top-0 left-0 w-1/2 sm:w-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-40 ${
-          isOpen ? "translate-x-0" : "-translate-x-full"
+        className={`fixed top-0 right-0 w-1/2 sm:w-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-40 ${
+          isOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
         <div className="p-6 h-full">
